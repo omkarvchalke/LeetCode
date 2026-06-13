3d8f19810506248b58fe8762b1ff469e7aa0dca9
omkarvchalke <48469799+omkarvchalke@users.noreply.github.com>
2026-06-13 16:55:48 -0500

    Time: 48 ms (36.91%), Space: 19.2 MB (56.68%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,15 +1,16 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        ans=""
+        ans=[]
         min_len = min(len(word1),len(word2))
 
         for i in range(min_len):
-            ans = ans + word1[i] + word2[i]
+            ans.append(word1[i])
+            ans.append(word2[i])
         
         if len(word1)>len(word2):
-            ans = ans + word1[min_len:]
+            ans.append(word1[min_len:])
         else:
-            ans = ans + word2[min_len:]
-        return ans
+            ans.append(word2[min_len:])
+        return "".join(ans)
 
         #Time and Space O(m+n) -> appending string takes space
