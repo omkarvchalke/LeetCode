1008ed04f838d64aa7686d6a389f787fe7d4a24d
omkarvchalke <48469799+omkarvchalke@users.noreply.github.com>
2026-06-16 23:26:04 -0500

    Time: 2 ms (55.9%), Space: 20.5 MB (18.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_map={}
+        h={} #hashmap of {value:index} from list nums
+        for i in range(len(nums)): #adding last index of specific value in hashmap
+            h[nums[i]] = i
+        
         for i in range(len(nums)):
-            comp = target - nums[i]
-            if comp in nums_map:
-                return [nums_map[comp],i]
-            nums_map[nums[i]] = i
+            y = target - nums[i] #y is the 2nd number from nums 
+
+            if y in h and h[y]!=i:
+                return [i,h[y]]
+
+
+        
